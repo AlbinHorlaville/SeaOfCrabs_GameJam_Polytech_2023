1332e4eb619fc0276b15a6578149009ee9aa3262
AlbinHorlaville <albinhorlaville@gmail.com>
2023-06-09 15:33:29 +0200

    Ajout Get/Set

--- game-project/game/info3/game/vue/toolkitUI/UIComponent.java
+++ game-project/game/info3/game/vue/toolkitUI/UIComponent.java
@@ -7,13 +7,45 @@ public abstract class UIComponent {
 	private int positionY;
 	private int height;
 	private int width;
-	
+
 	public UIComponent(int x, int y, int h, int w) {
 		this.positionX = x;
 		this.positionY = y;
 		this.height = h;
-		this.width =  w;
+		this.width = w;
 	}
-	
+
 	public abstract void paint(Graphics g);
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
 }
