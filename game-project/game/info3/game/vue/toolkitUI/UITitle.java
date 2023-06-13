9d5ad59e7ba6ed985e4d32239c84db43eba71b04
AlbinHorlaville <albinhorlaville@gmail.com>
2023-06-13 11:13:12 +0200

    Toolkit presque fini, ajout fonction readFile pour le score et les crédits

---
+++ game-project/game/info3/game/vue/toolkitUI/UITitle.java
@@ -0,0 +1,27 @@
+package info3.game.vue.toolkitUI;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+public class UITitle extends UILabel{
+
+	public UITitle(int widthWindow, int heightWindow, String text, Font font, Color fg) {
+		super(widthWindow, heightWindow, text, font, fg);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void paint(Graphics g) {
+		g.setFont(this.getFont());
+		int labelWidth = g.getFontMetrics().stringWidth(this.getText());
+		int labelHeight = g.getFontMetrics().getHeight();
+		
+		int x = (this.getPositionX() - labelWidth)/2; // Ici X et Y servent à connaître la taille de la fenêtre
+		int y = (int) (labelHeight*1.5);
+		
+		g.setColor(getFontColor());
+		g.setFont(getFont());
+		g.drawString(getText(), x, y);
+	}
+
+}
---
+++ game-project/game/info3/game/vue/view/CreditsView.java
@@ -0,0 +1,99 @@
+package info3.game.vue.view;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import info3.game.GameState;
+import info3.game.vue.GameView;
+import info3.game.vue.toolkitUI.UIButton;
+import info3.game.vue.toolkitUI.UIComponentListener;
+import info3.game.vue.toolkitUI.UILabel;
+import info3.game.vue.toolkitUI.UITitle;
+
+public class CreditsView extends View{
+	
+	UIButton buttonRetour;
+	UITitle title;
+	ArrayList<UILabel> listCredits;
+	UILabel credits;
+
+	public CreditsView(GameView gv) {
+		super(gv);
+		
+		buttonRetour = new UIButton(100, 600, 200, new UILabel(0, 0, "Retour", FONT1, c1), c2);
+		title = new UITitle(1024, 768, "Credits", FONT2, Color.white);
+		/*noms1 = new UILabel(200, 250, 
+				"Romain MIRAS,"
+				+ " Albin HORLAVILLE,"
+				+ " Axel COLE, "
+				+ " Brice DECURNINGE,", FONT1, Color.white);
+		noms2 = new UILabel(250, 300, 
+				" Alexandre ARLE,"
+				+ " Rémi DEL MEDICO,"
+				+ " Emineh GUNDOGAN", FONT1, Color.white);*/
+		//credits = new String(new File());
+		listCredits = readCreditsFile();
+		
+		buttonRetour.setUIComponentListener(new UIComponentListener() {
+			@Override
+			public void onComponentClicked() {
+				gameView.update_view(GameState.Menu);
+				gameView.getGame().setCurrentState(GameState.Menu);
+			}
+
+			@Override
+			public void onComponentMouseIn() {
+				buttonRetour.setBackgroundColor(c1);
+				buttonRetour.setForegroundColor(c2);
+			}
+
+			@Override
+			public void onComponentMouseOut() {
+				buttonRetour.setBackgroundColor(c2);
+				buttonRetour.setForegroundColor(c1);
+			}
+
+			public void onComponentPressed(int x, int y) {
+			};
+
+		});
+		
+		addComponent(buttonRetour);
+		addComponent(title);
+		for (UILabel x : listCredits) {
+			addComponent(x);
+		}
+	}
+	
+	ArrayList<UILabel> readCreditsFile() {
+		try {
+		      File credits = new File("resources/Credits");
+		      Scanner myReader = new Scanner(credits);
+		      ArrayList<UILabel> listeCredits = new ArrayList<UILabel>();
+		      int i=0;
+		      while (myReader.hasNextLine()) {
+		        String line = myReader.nextLine();
+		        UILabel lineLabel = new UILabel(250, 250+i, line, FONT1, Color.white);
+		        i+=50;
+		        listeCredits.add(lineLabel);
+		      }
+		      myReader.close();
+		      
+		      return listeCredits;
+	    } catch (FileNotFoundException e) {
+	      System.out.println("An error occurred.");
+	      e.printStackTrace();
+	    }
+		return null;
+	}
+
+	@Override
+	public void tick(long elapsed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
