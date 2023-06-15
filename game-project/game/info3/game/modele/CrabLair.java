b905477a66ee5e1727713d67029eb309e13f48cc
RDel-Medico <redelmedico@gmail.com>
2023-06-15 11:14:52 +0200

    Added crabLair and treasur generation function

--- game-project/game/info3/game/modele/CrabLair.java
+++ game-project/game/info3/game/modele/CrabLair.java
@@ -1,15 +1,19 @@
 package info3.game.modele;
 
+import info3.game.modele.map.MapSection;
+
 public class CrabLair extends StillEntity{
 	
 	protected int nbCrabs;
 	protected int crabsLvl;
 	protected int lifePoint;
+	private MapSection section;
 	
-	public CrabLair(int nbCrabs, int crabsLvl, int lifePoint) {
+	public CrabLair(int nbCrabs, int crabsLvl, int lifePoint, MapSection section) {
 		this.nbCrabs = nbCrabs;
 		this.crabsLvl = crabsLvl;
 		this.lifePoint = lifePoint;
+		this.section = section;
 	}
 	
 	
