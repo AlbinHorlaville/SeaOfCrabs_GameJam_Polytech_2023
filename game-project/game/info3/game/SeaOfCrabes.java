b9f75db3918ebfebf2c0339592f6ff2f940958f7
arlea <alexandre.arle@etu.univ-grenoble-alpes.fr>
2023-06-20 09:12:52 +0200

    fixing sql imports

--- game-project/game/info3/game/SeaOfCrabes.java
+++ game-project/game/info3/game/SeaOfCrabes.java
@@ -1,15 +1,7 @@
 package info3.game;
 
-import java.sql.DriverManager;
-
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Driver;
-import com.mysql.jdbc.ResultSet;
-import com.mysql.jdbc.Statement;
-
 import automate.AutomateLoader;
 import info3.game.modele.GameModele;
-import info3.game.sound.SoundTool;
 import info3.game.vue.GameView;
 import info3.game.vue.SpriteLoader.SpriteLoader;
 
