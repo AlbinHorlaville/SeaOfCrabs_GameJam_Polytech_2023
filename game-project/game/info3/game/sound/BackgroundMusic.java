3697d3a5f87f327bc269949df910ae22b93b1e9c
Alexandre Arle <alexandre.arle@etu.univ-grenoble-alpes.fr>
2023-06-21 13:46:11 +0200

    Revert "Merge branch 'mirasr.OptiSound' into 'dev'"
    This reverts merge request !123

--- game-project/game/info3/game/sound/BackgroundMusic.java
+++ game-project/game/info3/game/sound/BackgroundMusic.java
@@ -1,17 +1,6 @@
 package info3.game.sound;
 
 public enum BackgroundMusic {
-	
-	MainMenu("Town8.ogg"),
-	Game("MainTheme.ogg");
-
-	private final String fileName;
-
-	private BackgroundMusic(String string) {
-		this.fileName = string;
-	}
-	
-	public String getFileName() {
-		return fileName;
-	}
+	MainMenu,
+	Game
 }
--- game-project/game/info3/game/sound/SoundEffect.java
+++ game-project/game/info3/game/sound/SoundEffect.java
@@ -1,16 +1,6 @@
 package info3.game.sound;
 
 public enum SoundEffect {
-	SwordSound("Sword4.ogg"),
-	Test("Sword4.ogg");
-
-	private final String fileName;
-
-	SoundEffect(String string) {
-		this.fileName = string;
-	}
-	
-	public String getFileName() {
-		return fileName;
-	}
+	SwordSound,
+	Test
 }
