ecff9f4923f53a8d32b98bb1c253ce26302a0690
RomainMIRAS <73794188+RomainMIRAS@users.noreply.github.com>
2023-06-22 16:19:12 +0200

    StunningAvatar

--- game-project/game/info3/game/modele/MoveableEntityClass/StunningCannonBall.java
+++ game-project/game/info3/game/modele/MoveableEntityClass/StunningCannonBall.java
@@ -1,12 +1,12 @@
 package info3.game.modele.MoveableEntityClass;
 
-import info3.game.vue.avatar.BasicCannonBallAvatar;
+import info3.game.vue.avatar.StunningCannonBallAvatar;
 
 public class StunningCannonBall extends CannonBall {
 	
 	public StunningCannonBall() {
 		super(0, BASIC_RANGE, BASIC_RATE_OF_FIRE);
-		this.setAvatar(new BasicCannonBallAvatar(this));
+		this.setAvatar(new StunningCannonBallAvatar(this));
 		this.r = this.avatar.getWidth();
 	}
 
